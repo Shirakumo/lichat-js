3363e2e1186868b58ccb5be3164def8c7ae8c3c2
Shinmera <shinmera@tymoon.eu>
2017-01-22 11:45:27 +0100

    Fix getUniversalTime for real.

--- cl.js
+++ cl.js
@@ -257,7 +257,7 @@ var CL = function(){
     self.universalUnixOffset = 2208988800;
 
     self.getUniversalTime = ()=>{
-        return (Date.now()/1000)+self.universalUnixOffset;
+        return Math.round(Date.now()/1000)+self.universalUnixOffset;
     };
 
     self.universalToUnix = (universal)=>{
