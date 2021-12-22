bb4a9eefecdb4859ee67c4edddcc08a23edc17f2
Janne Pakarinen <gingeralesy@gmail.com>
2021-12-22 11:08:52 +0200

    Moved static fields outside of the class block

--- ui.js
+++ ui.js
@@ -794,53 +794,6 @@ class LichatUI{
         tx.onerror = (ev)=>console.error(ev);
     }
 
-    static allEmoji = {};
-
-    static sound = new Audio('notify.mp3');
-    
-    // URL Regex by Diego Perini: https://gist.github.com/dperini/729294
-    static URLRegex = new RegExp(
-        "^" +
-            // protocol identifier (optional)
-        // short syntax // still required
-        "(?:(?:(?:https?|ftp):)?\\/\\/)" +
-            // user:pass BasicAuth (optional)
-        "(?:\\S+(?::\\S*)?@)?" +
-            "(?:" +
-            // IP address exclusion
-        // private & local networks
-        "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
-            "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
-            "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-            // IP address dotted notation octets
-        // excludes loopback network 0.0.0.0
-        // excludes reserved space >= 224.0.0.0
-        // excludes network & broadcast addresses
-        // (first & last IP address of each class)
-        "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
-            "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
-            "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
-            "|" +
-            // host & domain names, may end with dot
-        // can be replaced by a shortest alternative
-        // (?![-_])(?:[-\\w\\u00a1-\\uffff]{0,63}[^-_]\\.)+
-        "(?:" +
-            "(?:" +
-            "[a-z0-9\\u00a1-\\uffff]" +
-            "[a-z0-9\\u00a1-\\uffff_-]{0,62}" +
-            ")?" +
-            "[a-z0-9\\u00a1-\\uffff]\\." +
-            ")+" +
-            // TLD identifier name, may end with dot
-        "(?:[a-z\\u00a1-\\uffff]{2,}\\.?)" +
-            ")" +
-            // port number (optional)
-        "(?::\\d{2,5})?" +
-            // resource path (optional)
-        "(?:[/?#]\\S*)?" +
-            "$", "i"
-    );
-
     static linkifyURLs(text){
         let out = [];
         let word = [];
@@ -968,7 +921,50 @@ class LichatUI{
         return LichatUI.replaceEmotes(LichatUI.markSelf(LichatUI.linkifyURLs(LichatUI.escapeHTML(text)), channel), channel);
     }
 }
-
+LichatUI.allEmoji = {};
+LichatUI.sound = new Audio('notify.mp3');
+// URL Regex by Diego Perini: https://gist.github.com/dperini/729294
+LichatUI.URLRegex = new RegExp(
+    "^" +
+        // protocol identifier (optional)
+    // short syntax // still required
+    "(?:(?:(?:https?|ftp):)?\\/\\/)" +
+        // user:pass BasicAuth (optional)
+    "(?:\\S+(?::\\S*)?@)?" +
+        "(?:" +
+        // IP address exclusion
+    // private & local networks
+    "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+        "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+        "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+        // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broadcast addresses
+    // (first & last IP address of each class)
+    "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+        "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+        "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+        "|" +
+        // host & domain names, may end with dot
+    // can be replaced by a shortest alternative
+    // (?![-_])(?:[-\\w\\u00a1-\\uffff]{0,63}[^-_]\\.)+
+    "(?:" +
+        "(?:" +
+        "[a-z0-9\\u00a1-\\uffff]" +
+        "[a-z0-9\\u00a1-\\uffff_-]{0,62}" +
+        ")?" +
+        "[a-z0-9\\u00a1-\\uffff]\\." +
+        ")+" +
+        // TLD identifier name, may end with dot
+    "(?:[a-z\\u00a1-\\uffff]{2,}\\.?)" +
+        ")" +
+        // port number (optional)
+    "(?::\\d{2,5})?" +
+        // resource path (optional)
+    "(?:[/?#]\\S*)?" +
+        "$", "i"
+);
 
 (()=>{
     let request = new XMLHttpRequest();
